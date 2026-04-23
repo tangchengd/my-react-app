385fd3c835b489a12f296ad173ccde8446eb56e3
Tcd <2226280391@qq.com>
2026-04-23 14:31:29 +0800

    feat:滑动优化

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import DigitalClock from "../../components/DigitalClock/DigitalClock";
 import QuotesSection from "../../components/QuotesSection/QuotesSection";
 import "./Home.css";
 
 export default function Home() {
+  const [hasScrolled, setHasScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      setHasScrolled(window.scrollY > 20);
+    }
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <section className="home-page">
       <div className="home-hero">
         <DigitalClock />
-        <div className="home-scroll-hint" aria-hidden="true">
-          {/* <span>向下探索</span> */}
+        <div className={`home-scroll-hint${hasScrolled ? " home-scroll-hint-hidden" : ""}`} aria-hidden="true">
+          <span>向下探索</span>
           <span className="home-scroll-hint-arrow">↓</span>
         </div>
       </div>
