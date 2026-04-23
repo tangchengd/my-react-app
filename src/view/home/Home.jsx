bc35cc4badddc9bae94c8d16d4aaf1834c2eb24b
Tcd <2226280391@qq.com>
2026-04-23 14:08:22 +0800

    feat:滑动图标

--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,10 +1,21 @@
 import DigitalClock from "../../components/DigitalClock/DigitalClock";
+import QuotesSection from "../../components/QuotesSection/QuotesSection";
 import "./Home.css";
 
 export default function Home() {
   return (
     <section className="home-page">
-      <DigitalClock />
+      <div className="home-hero">
+        <DigitalClock />
+        <div className="home-scroll-hint" aria-hidden="true">
+          {/* <span>向下探索</span> */}
+          <span className="home-scroll-hint-arrow">↓</span>
+        </div>
+      </div>
+
+      <div className="home-quotes">
+        <QuotesSection />
+      </div>
     </section>
   );
 }
